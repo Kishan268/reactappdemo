5587a09437854c027e58dac5b81c9523d19ead07
Mukesh <m.kushwah33@gmail.com>
2022-04-25 12:26:58 +0530

    change package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import ReactPlayer from 'react-player'
+
 function App() {
   return (
     <div className="App">
-         <video playsInline loop muted autoPlay 
+         {/* <video playsInline loop muted autoPlay 
           style={{height:'100%', width:'100%'}}
          >
           <source  src='https://evergrowcoin.com/wp-content/uploads/2022/04/coming-soon-lunasky.mp4' type="video/mp4" />
-        </video>
+        </video> */}
+        <ReactPlayer
+            url="https://evergrowcoin.com/wp-content/uploads/2022/04/coming-soon-lunasky.mp4"
+            width="100%"
+            height="100%"
+            playing
+            loop
+            playsinline
+            muted
+            config={{
+              file: {
+                attributes: {
+                  preload: "auto",
+                },
+              },
+            }}
+          />
+
     </div>
   );
 }
