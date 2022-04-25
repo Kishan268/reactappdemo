d5bd6df25ad178f383c8e918c972b547742ec5b4
kishan3494 <kishans.edigillence@gmail.com>
2022-04-25 10:59:18 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import logo from './logo.svg';
 import lunasky from './coomingsoonlunasky.mp4'
-// import './App.css';
-// import "video-react/dist/video-react.css";
 
 import { Player } from 'video-react';
 
 function App() {
   return (
     <div className="App">
-        {/* <Player
-          playsInline
-          poster="/assets/poster.png"
-          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-        /> */}
+
          <video  loop muted autoPlay  
           style={{height:'100%', width:'100%'}}
          >
