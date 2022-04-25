66c992dcb70f6bbf638b2dc160d72e985e2cf9dc
Mukesh <m.kushwah33@gmail.com>
2022-04-25 12:08:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 function App() {
   return (
     <div className="App">
-
-         <video  loop muted autoPlay playsinline 
+         <video playsInline loop muted autoPlay 
           style={{height:'100%', width:'100%'}}
          >
           <source  src='https://evergrowcoin.com/wp-content/uploads/2022/04/coming-soon-lunasky.mp4' type="video/mp4" />
