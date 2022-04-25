0ade57e5b61b91764df04db0ebb569ac32e2a819
kishan3494 <kishans.edigillence@gmail.com>
2022-04-25 10:54:24 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import lunasky from './coomingsoonlunasky.mp4'
+// import './App.css';
+// import "video-react/dist/video-react.css";
+
+import { Player } from 'video-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <Player
+          playsInline
+          poster="/assets/poster.png"
+          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+        /> */}
+         <video  loop muted autoPlay  
+          style={{height:'100%', width:'100%'}}
+         >
+          <source  src={lunasky} type="video/mp4"/>
+        </video>
     </div>
   );
 }
